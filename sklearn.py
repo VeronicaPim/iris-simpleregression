300a53a7d5d8d1179f3d0d3bd638781dddb6c392
Veronica Pimenova <96704800+VeronicaPim@users.noreply.github.com>
2024-05-07 11:10:04 -0400

    Update sklearn.py

--- sklearn.py
+++ sklearn.py
@@ -11,6 +11,7 @@ X, y = iris.data, iris.target
 #splitting the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+
 #initializing and training the logistic regression model
 model = LogisticRegression(max_iter=200)
 model.fit(X_train, y_train)
