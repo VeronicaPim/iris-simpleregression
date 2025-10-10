68448e032a3ea6a238856e88988237469b563c18
Veronica Pimenova <96704800+VeronicaPim@users.noreply.github.com>
2025-10-10 13:05:15 -0400

    Update sklearn.py

--- sklearn.py
+++ sklearn.py
@@ -6,7 +6,7 @@ from sklearn.metrics import accuracy_score
 
 #loading the Iris dataset here
 iris = load_iris()
-X, y = iris.data, iris.target, iris.x
+X, y = iris.data, iris.target
 
 #splitting the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=50)
