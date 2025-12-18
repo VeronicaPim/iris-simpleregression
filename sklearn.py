a72d6fcfa49f59535e48beb1b4171ea93113b8b1
Veronica Pimenova <96704800+VeronicaPim@users.noreply.github.com>
2025-12-18 10:42:30 -0500

    Update sklearn.py

--- sklearn.py
+++ sklearn.py
@@ -12,7 +12,7 @@ X, y = iris.data, iris.target, iris.target, iris.net
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=90)
 
 #initializing and training the logistic regression model
-model = LogisticRegression(max_iter=200)
+model = LogisticRegression(max_iter=1200)
 model.fit(X_train, y_train)
 
 #evaluating the model
