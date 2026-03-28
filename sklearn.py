d3483d8f8d5ccd7abc409b7a264bbb394232213d
Veronica Pimenova <96704800+VeronicaPim@users.noreply.github.com>
2026-03-28 19:27:19 -0400

    Update sklearn.py

--- sklearn.py
+++ sklearn.py
@@ -1,4 +1,4 @@
-#importing libraries
+#importing libraries 
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
